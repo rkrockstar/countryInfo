56f66ff65bf77894d237a1b55e737db9fbfe1f91
LAPTOP-Rounak Kulkarni <rounakkulkarni@csu.fullerton.edu>
2018-03-19 14:00:57 -0700

    4

--- index.js
+++ index.js
@@ -14,49 +14,54 @@ restService.use(
 restService.use(bodyParser.json());
 
 
+restService.post('/countryInfo', function(req, res) {
+ //var speech = req.params.countryName
+ app.post('https://restcountries.eu/rest/v2/name', function(req, res) {
 
-app.post('https://restcountries.eu/rest/v2/name/{countryName}', function(req, res) {
-
-    var countryDetail = {}
-
-    countryDetail.countryName = req.params.countryName;
+  var countryDetail = req.param('countryName');
+   //countryDetail.countryName = req.params.countryName;
     
 
-  //  res.send(countryDetail); // send json response
-     return res.json({
+  res.send(countryDetail); // send json response
+    return res.json({
     speech: countryDetail,
-    displayText: countryDetail,
+   displayText: countryDetail,
     source: "countryInfo-sample"
   });
 });
-
-app.listen(443);
-
-
-
-//var options = {
+    
+});
+	  
+ 
+	
+  //return res.json({
+//    speech: speech,
+  //  displayText: speech,
+//    source: "webhook-echo-sample"
+//  });
+//});
+    
+    //var options = {
  // host: 'https://restcountries.eu/rest/v2/name',
  // port: 443,
  // path: '/{countryName}',
  // method: 'POST'
 //};
 
+//app.post('https://restcountries.eu/rest/v2/name/{countryName}', function(req, res) {
 
-//restService.post(options, function(req, res) {
- // var speech =
- //   req.body.result &&
- //   req.body.result.parameters &&
- //   req.body.result.parameters.echoText
- //     ? req.body.result.parameters.echoText
- //     : "Seems like some problem. Speak again.";
-	  
- 
-	
- // return res.json({
- //   speech: speech,
- //   displayText: speech,
- //   source: "webhook-echo-sample"
+ //   var countryDetail = {}
+
+//   countryDetail.countryName = req.params.countryName;
+    
+
+  //  res.send(countryDetail); // send json response
+ //    return res.json({
+ //   speech: countryDetail,
+ //   displayText: countryDetail,
+ //   source: "countryInfo-sample"
  // });
 //});
 
+//app.listen(443);
 
