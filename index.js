e57d5955fe805200559632940ab6542d9428e22d
LAPTOP-Rounak Kulkarni <rounakkulkarni@csu.fullerton.edu>
2018-03-21 10:03:43 -0700

    10

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.post('/', function(req, res) {
-    if(req.body.result.parameters.text == "organisation"){
+app.post('/country', function(req, res) {
+    if(req.body.result.parameters.text == "country"){
         request.get({
-              url: 'https://restcountries.eu/rest/v2/name/canada';
-              headers: {
-                'Authorization': 'Token'
-              }
+              url: 'https://restcountries.eu/rest/v2/name/{country}';
+              //headers: {
+            //    'Authorization': 'Token'
+            //  }
             }, function(error, response, body) {
                 var info = JSON.parse(body);
                 console.log(info);
                 var resp = "Country Info :" + info.name;
-                resp += ",Capital is : " +  info.capital;
-                resp += ",Population is : " +  info.population;
+           //     resp += ",Capital is : " +  info.capital;
+            //    resp += ",Population is : " +  info.population;
                 //var reqy = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
                 res.json({
                     speech: resp,
                     displayText: resp,
-                    source: 'webhook'
+                    source: 'echo-rounak'
             });
         });
     }
